519a6baaa6f44960dcccb4c4d583bbeec878f3b4
dounia-elg <dounia.elg20@gmail.com>
2026-02-27 11:28:47 +0000

    feat: update tab layout and remove unused screens; add async storage dependency

---
+++ app/(tabs)/parcelles.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ParcellesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Parcelles ✅</Text>
+    </View>
+  );
+}
